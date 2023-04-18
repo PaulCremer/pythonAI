89c5158886015f1cca0a8d01ca953a56b45b5f07
PaulCremer <paul.cremer@protonmail.com>
2023-04-18 22:55:48 +0200

    added new file structure

---
+++ CNN.py
@@ -0,0 +1,5 @@
+# the ConvolutionalNeuralNetwork-class will be coded here
+
+class ConvolutionalNeuralNetwork:
+    def printTest(self, text):
+        print(text)
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def print_works(thing):
-    print(f'{thing} works')
-
+from CNN import ConvolutionalNeuralNetwork
 
 if __name__ == '__main__':
-    print_works('test')
+    CNN = ConvolutionalNeuralNetwork()
+    CNN.printTest("Test")
